2240bc6d7f32e0f49b0f41105eddb27398e4e0e3
Ernestas Zaglinskas <virtualybe@gmail.com>
2023-05-24 13:54:14 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-$krn     = shell_exec('uname -r');
-$uptime  = shell_exec('uptime -p');
-$hdd     = shell_exec('df / -h');
-$lsblk   = shell_exec('lsblk');
-$mem     = shell_exec('free -h');
-$cpu     = shell_exec("top -n 1 -b | awk '/^%Cpu/{print $2}'");
-$corenum = shell_exec("nproc");
-$wifi    = shell_exec('nmcli dev wifi');
+$krn     = shell_exec('uname -r');   // Linux kernel info
+$uptime  = shell_exec('uptime -p');  // System uptime info
+$hdd     = shell_exec('df / -h');    // Partition info
+$lsblk   = shell_exec('lsblk');      // Disks and partitions info
+$mem     = shell_exec('free -h');    // Memory RAM info
+$cpu     = shell_exec("top -n 1 -b | awk '/^%Cpu/{print $2}'"); // CPU info
+$corenum = shell_exec("nproc");      // Count cores
+$wifi    = shell_exec('nmcli dev wifi');  // Wifi info, available networks
 ?>

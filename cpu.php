df7ec2ab5d6db4c1bc2d2aa3e2301181d712d0a6
Ernestas Zaglinskas <virtualybe@gmail.com>
2023-08-23 13:48:00 +0300

    Update cpu.php
    add cpu temp

--- cpu.php
+++ cpu.php
@@ -1,6 +1,10 @@
 <?php
-$cpu     = shell_exec("top -n 1 -b | awk '/^%Cpu/{print $2}'");
-$corenum = shell_exec("nproc");
+$cpu      = shell_exec("top -n 1 -b | awk '/^%Cpu/{print $2}'");
+$corenum  = shell_exec("nproc");
+$cputemp0 = shell_exec("sensors | grep -A 0 'Core 0' | cut -c1-25");
+$cputemp1 = shell_exec("sensors | grep -A 0 'Core 1' | cut -c1-25");
 echo "CPU cores: $corenum<br>";
-echo "Use: $cpu %";
+echo "Use: $cpu %<br>";
+echo "$cputemp0<br>";
+echo "$cputemp1";
 ?>
